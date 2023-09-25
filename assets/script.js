9efcc7afc335141170d509abd7f3fb7787b30930
jakobpfeifer135 <138628595+jakobpfeifer135@users.noreply.github.com>
2023-09-25 15:20:33 -0500

    changed the question formatting in js

--- assets/script.js
+++ assets/script.js
@@ -1,80 +1,113 @@
 var score = 0;
-var timeLeft = 120; // 2 minutes
+var timeLeft = 60; // 1 minute
 var currentQuestion = 1;
 var answeredQuestions = {};
 
+
+var questions = [
+  {
+    questionTitle: "In magic the gathering how many colors are there",
+    choices: ["4", "5", "3", "6"],
+    answer: "5",
+  },
+  {
+    questionTitle: "in magic the gathering when can you cast sorceries",
+    choices: [
+      "on your turn and your opponents turn",
+      "on only your opponents turn",
+      "on only your turn",
+      "only at the beginning of your turn",
+    ],
+    answer: "on only your turn",
+  },
+  {
+    questionTitle:
+      "in magic the gathering you lose when you take how much commander damage",
+    choices: ["20", "21", "15", "10"],
+    answer: "21",
+  },
+  {
+    questionTitle: "in magic the gathering in what order does your turn begin",
+    choices: [
+      "draw, upkeep, un-tap",
+      "upkeep, draw, un-tap",
+      "upkeep, un-tap, draw",
+      "un-tap, upkeep, draw",
+    ],
+    answer: "un-tap, upkeep, draw",
+  },
+  {
+    questionTitle:
+      "what color in magic the gathering is most known for life-gain?",
+    choices: ["blue", "red", "white", "green"],
+    answer: "white",
+  },
+];
+
 function updateTimer() {
-    const timerElement = document.getElementById("quizTimer");
-    const minutes = Math.floor(timeLeft / 60);
-    const seconds = timeLeft % 60;
-    //This line of code is used to update the text content of an HTML element with the id "quizTimer" to display the remaining time in minutes and seconds.
-    timerElement.textContent = `Time Left: ${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
-    //this allows the quiz to end once the time hits 0
-    if (timeLeft === 0) {
-        clearInterval(timerInterval);
-        endQuiz();
-    } else {
-        timeLeft--;
-    }
+  const timerElement = document.getElementById("quizTimer");
+  const minutes = Math.floor(timeLeft / 60);
+  const seconds = timeLeft % 60;
+  //This line of code is used to update the text content of an HTML element with the id "quizTimer" to display the remaining time in minutes and seconds.
+  timerElement.textContent = `Time Left: ${minutes}:${
+    seconds < 10 ? "0" : ""
+  }${seconds}`;
+  //this allows the quiz to end once the time hits 0
+  if (timeLeft === 0) {
+    clearInterval(timerInterval);
+    endQuiz();
+  } else {
+    timeLeft--;
+  }
 }
 // this should return an alert if they try to answer the question more than once
 
 function submitAnswer(questionId) {
-    if (answeredQuestions[questionId]) {
-        alert("You have already answered this question.");
-        return;
-    }
-// selecting the css element and storing the users selected answer
-// it selects the user's chosen answer to the current question by looking for the checked input element with a specific name attribute 
-    const selectedAnswer = document.querySelector(`input[name="${questionId}"]:checked`);
+  if (answeredQuestions[questionId]) {
+    alert("You have already answered this question.");
+    return;
+  }
+  // selecting the css element and storing the users selected answer
+  // it selects the user's chosen answer to the current question by looking for the checked input element with a specific name attribute
+  const selectedAnswer = document.querySelector(
+    `input[name="${questionId}"]:checked`
+  );
 
-    if (selectedAnswer) {
-        const correctAnswers = {
-            "q1": ["b"], // Add correct answers for each 
-            "q2": ["c"]
+  
 
-        };
-
-        const feedbackElement = document.createElement("p");
-        feedbackElement.textContent = `Your answer is ${correctAnswers[questionId].includes(selectedAnswer.value) ? "correct" : "incorrect"}.`;
-
-        const questionElement = document.getElementById(questionId);
-        questionElement.appendChild(feedbackElement);
-
-        if (correctAnswers[questionId].includes(selectedAnswer.value)) {
-            score++;
-        } else {
-            timeLeft -= 15; // Deduct 15 seconds for incorrect answers
-        }
-
-        answeredQuestions[questionId] = true;
-//sets up a timer to execute this code after a 2 second delay
-        setTimeout(() => {
-            currentQuestion++;
-            if (currentQuestion <= 2) {
-                const nextQuestionId = `question${currentQuestion}`;
-                document.getElementById(questionId).style.display = "none";
-                document.getElementById(nextQuestionId).style.display = "block";
-            } else {
-                endQuiz();
-            }
-        }, 2000); // Display feedback for 2 seconds before moving to the next question
+    if (correctAnswers[questionId].includes(selectedAnswer.value)) {
+      score++;
     } else {
-        alert("Please select an answer before submitting.");
+      timeLeft -= 15; // Deduct 15 seconds for incorrect answers
     }
-}
+
+    answeredQuestions[questionId] = true;
+    //sets up a timer to execute this code after a 2 second delay
+    setTimeout(() => {
+      currentQuestion++;
+      if (currentQuestion <= 2) {
+        const nextQuestionId = `question${currentQuestion}`;
+        document.getElementById(questionId).style.display = "none";
+        document.getElementById(nextQuestionId).style.display = "block";
+      } else {
+        endQuiz();
+      }
+    }, 2000); // Display feedback for 2 seconds before moving to the next question
+  }
+  
+
 //function to end the quiz once questions have been answered
 function endQuiz() {
-    clearInterval(timerInterval);
+  clearInterval(timerInterval);
 
-    // Display the final score
-    const quizScore = document.getElementById("quizScore");
-    quizScore.textContent = `Your Score: ${score}`;
+  // Display the final score
+  const quizScore = document.getElementById("quizScore");
+  quizScore.textContent = `Your Score: ${score}`;
 
-    // Hide the remaining questions
-    document.getElementById("question1").style.display = "none";
-    document.getElementById("question2").style.display = "none";
-}
+  // Hide the remaining questions
+//   document.getElementById("question1").style.display = "none";
+//   document.getElementById("question2").style.display = "none";
+ }
 
 const timerInterval = setInterval(updateTimer, 1000);
 
@@ -104,31 +137,21 @@ const timerInterval = setInterval(updateTimer, 1000);
 // }
 
 // var secondQuestion = {
-// question2Option1 : "on your turn and your opponents turn",
-// question2Option2 : "on only your opponents turn",
-// question2Option3 : "on only your turn",
-// question2Option4 : "only at the beginning of your turn",
+//  "on your turn and your opponents turn",
+//  "on only your opponents turn",
+//  "on only your turn",
+//  "only at the beginning of your turn",
 
 // }
 
 // var thirdQuestion = {
-// question3Option1 : "20",
-// question3Option2 : "21",
-// question3Option3 : "15",
-// question3Option4 : "10"
+
 // }
 
 // var fourthQuestion = {
-// question4Option1 : "draw, upkeep, un-tap",
-// question4Option2 : "upkeep, draw, un-tap",
-// question4Option3 : "upkeep, un-tap, draw",
-// question4Option4 : "un-tap, upkeep, draw",
+
 // }
 
 // var fifthQuestion = {
-// question5Option1 : "blue",
-// question5Option2 : "red",
-// question5Option3 : "white",
-// question5Option4 : "green",
-// }
 
+// }
