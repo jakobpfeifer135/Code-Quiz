503ebd5cf6a44e9eafdf08246f3337ce90af134c
jakobpfeifer135 <138628595+jakobpfeifer135@users.noreply.github.com>
2023-09-27 16:54:13 -0500

    added deployed link to README

--- assets/script.js
+++ assets/script.js
@@ -39,8 +39,7 @@ var questions = [
     answer: "un-tap, upkeep, draw",
   },
   {
-    question:
-      "What color in magic the gathering is most known for life-gain?",
+    question: "What color in magic the gathering is most known for life-gain?",
     choices: ["blue", "red", "white", "green"],
     answer: "white",
   },
@@ -58,49 +57,49 @@ startButton.addEventListener("click", function () {
 
 // Function to start the timer
 function startTimer() {
-    var timer = setInterval(function () {
-      timeLeft--;
-      timerElement.textContent = timeLeft;
-  
-      if (timeLeft <= 0) {
-        clearInterval(timer);
-        endQuiz();
+  var timer = setInterval(function () {
+    timeLeft--;
+    timerElement.textContent = timeLeft;
+
+    if (timeLeft <= 0) {
+      clearInterval(timer);
+      endQuiz();
+    }
+  }, 1000);
+}
+
+// Function to display the current question and choices
+function displayQuestion() {
+  var questionElement = document.getElementById("question");
+  var choicesList = document.getElementById("choices");
+
+  var currentQuestion = questions[currentQuestionIndex];
+
+  // Display the question
+  questionElement.textContent = currentQuestion.question;
+
+  // Clear previous choices
+  choicesList.innerHTML = "";
+
+  // Display answer choices
+  currentQuestion.choices.forEach(function (choice, index) {
+    var listItem = document.createElement("li");
+    listItem.textContent = choice;
+    listItem.addEventListener("click", function () {
+      // Check if the selected choice is correct
+      if (choice === currentQuestion.answer) {
+        // Increment score if correct
+        score++;
+      } else {
+        // Deduct 10 seconds for incorrect answers
+        timeLeft -= 10;
       }
-    }, 1000);
-  }
-  
-  // Function to display the current question and choices
-  function displayQuestion() {
-    var questionElement = document.getElementById("question");
-    var choicesList = document.getElementById("choices");
-  
-    var currentQuestion = questions[currentQuestionIndex];
-  
-    // Display the question
-    questionElement.textContent = currentQuestion.question;
-  
-    // Clear previous choices
-    choicesList.innerHTML = "";
-  
-    // Display answer choices
-    currentQuestion.choices.forEach(function (choice, index) {
-      var listItem = document.createElement("li");
-      listItem.textContent = choice;
-      listItem.addEventListener("click", function () {
-        // Check if the selected choice is correct
-        if (choice === currentQuestion.answer) {
-          // Increment score if correct
-          score++;
-        } else {
-          // Deduct 10 seconds for incorrect answers
-          timeLeft -= 10;
-        }
-  
-        // Move to the next question
-        currentQuestionIndex++;
-  
-    // Check if there are more questions
-    if (currentQuestionIndex < questions.length) {
+
+      // Move to the next question
+      currentQuestionIndex++;
+
+      // Check if there are more questions
+      if (currentQuestionIndex < questions.length) {
         displayQuestion(); // Display the next question
       } else {
         endQuiz();
@@ -112,37 +111,37 @@ function startTimer() {
 }
 // Function to end the quiz
 function endQuiz() {
-    quizContainer.style.display = "none"; // Hide the quiz container
-    quizScoreElement.textContent = "Questions answered correctly: " + score;
-    storeHighScore();
-  }
-  
-  // Function to store high scores in local storage
-  function storeHighScore() {
-    var initials = prompt("Enter your initials (3 characters):");
-    if (initials && initials.length === 3) {
-      var scores = JSON.parse(localStorage.getItem("scores")) || [];
-      scores.push({ score: timeLeft, initials: initials });
-      scores.sort(function (a, b) {
-        return b.score - a.score;
-      });
-      localStorage.setItem("scores", JSON.stringify(scores));
-      displayHighScores();
-    } else {
-      alert("Please enter 3 initials.");
-    }
-  }
-  
-  // Function to display high scores from local storage
-  function displayHighScores() {
-    var highScoresList = document.getElementById("highScoresList");
-    highScoresList.innerHTML = "";
-  
+  quizContainer.style.display = "none"; // Hide the quiz container
+  quizScoreElement.textContent = "Questions answered correctly: " + score;
+  storeHighScore();
+}
+
+// Function to store high scores in local storage
+function storeHighScore() {
+  var initials = prompt("Enter your initials (3 characters):");
+  if (initials && initials.length === 3) {
     var scores = JSON.parse(localStorage.getItem("scores")) || [];
-    scores.forEach(function (scoreData, index) {
-      var listItem = document.createElement("li");
-      listItem.textContent = index + 1 + ". " + scoreData.initials + ": " + scoreData.score;
-      highScoresList.appendChild(listItem);
+    scores.push({ score: timeLeft, initials: initials });
+    scores.sort(function (a, b) {
+      return b.score - a.score;
     });
+    localStorage.setItem("scores", JSON.stringify(scores));
+    displayHighScores();
+  } else {
+    alert("Please enter 3 initials.");
   }
-  
+}
+
+// Function to display high scores from local storage
+function displayHighScores() {
+  var highScoresList = document.getElementById("highScoresList");
+  highScoresList.innerHTML = "";
+
+  var scores = JSON.parse(localStorage.getItem("scores")) || [];
+  scores.forEach(function (scoreData, index) {
+    var listItem = document.createElement("li");
+    listItem.textContent =
+      index + 1 + ". " + scoreData.initials + ": " + scoreData.score;
+    highScoresList.appendChild(listItem);
+  });
+}
